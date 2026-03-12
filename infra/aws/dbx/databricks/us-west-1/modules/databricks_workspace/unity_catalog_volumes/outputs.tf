cb7c4bb0065184284b9bc50c93bc609e7e22e9ee
Giuliano Altobelli <giulianoaltobelli@Mac.lan>
2026-03-12 01:00:25 -0700

    feat: add unity catalog volumes outputs

--- infra/aws/dbx/databricks/us-west-1/modules/databricks_workspace/unity_catalog_volumes/outputs.tf
+++ infra/aws/dbx/databricks/us-west-1/modules/databricks_workspace/unity_catalog_volumes/outputs.tf
@@ -1,2 +1,14 @@
-# Module outputs.
-
+output "volumes" {
+  description = "Managed Unity Catalog volumes keyed by stable caller-defined identifiers."
+  value = {
+    for volume_key, volume in databricks_volume.this :
+    volume_key => {
+      name             = volume.name
+      catalog_name     = local.enabled_volumes[volume_key].catalog_name
+      schema_name      = local.enabled_volumes[volume_key].schema_name
+      full_name        = volume.id
+      volume_type      = local.enabled_volumes[volume_key].volume_type
+      storage_location = local.enabled_volumes[volume_key].volume_type == "EXTERNAL" ? local.enabled_volumes[volume_key].storage_location : null
+    }
+  }
+}
