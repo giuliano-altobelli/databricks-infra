5f7b34e25291ad8235e3a7aff051c8e9acc150ed
Giuliano Altobelli <giulianoaltobelli@Mac.lan>
2026-04-17 13:21:38 -0700

    chore(terraform): verify databricks aws secret placeholder integration

--- infra/aws/dbx/databricks/us-west-1/root_s3_bucket.tf
+++ infra/aws/dbx/databricks/us-west-1/root_s3_bucket.tf
@@ -33,6 +33,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "root_storage_buck
   count  = local.enable_customer_managed_keys ? 0 : 1
   bucket = aws_s3_bucket.root_storage_bucket.bucket
   rule {
+    blocked_encryption_types = ["NONE"]
     bucket_key_enabled = true
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
